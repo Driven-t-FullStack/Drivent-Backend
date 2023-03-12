6b74607b12194082f720184832ceb39fe0d1eb86
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2023-03-12 20:08:28 -0300

    feat: redis in event route

--- src/services/events-service/index.ts
+++ src/services/events-service/index.ts
@@ -1,14 +1,24 @@
+import { redis } from "@/config";
 import { notFoundError } from "@/errors";
 import eventRepository from "@/repositories/event-repository";
 import { exclude } from "@/utils/prisma-utils";
 import { Event } from "@prisma/client";
 import dayjs from "dayjs";
 
-async function getFirstEvent(): Promise<GetFirstEventResult> {
-  const event = await eventRepository.findFirst();
-  if (!event) throw notFoundError();
+const HOURS_IN_SECONDS = 3600
 
-  return exclude(event, "createdAt", "updatedAt");
+async function getFirstEvent(): Promise<GetFirstEventResult> {
+  const cacheKey = "event";
+  const cachedEvent = await redis.get(cacheKey);
+  if (cachedEvent) {
+    const typedCache: Event = JSON.parse(cachedEvent)
+    return exclude(typedCache, "createdAt", "updatedAt")
+  } else {
+    const event = await eventRepository.findFirst();
+    if (!event) throw notFoundError();
+    redis.setEx(cacheKey, HOURS_IN_SECONDS, JSON.stringify(event));
+    return exclude(event, "createdAt", "updatedAt");
+  }
 }
 
 export type GetFirstEventResult = Omit<Event, "createdAt" | "updatedAt">;
